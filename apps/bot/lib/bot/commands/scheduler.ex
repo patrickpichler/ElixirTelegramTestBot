408277ae893cf6db6621a4b18e8067fff564ebf1
Patick Pichler <patri.pichler@gmail.com>
2017-08-12 12:12:19 +0200

    Rework the way the scheduler parses commands (WIP)

--- apps/bot/lib/bot/commands/scheduler.ex
+++ apps/bot/lib/bot/commands/scheduler.ex
@@ -1,27 +1,28 @@
 defmodule Bot.Commands.Scheduler do
   use Bot.Commander
+  import OK, only: ["~>>": 2]
 
   require OK
 
   def schedule(update) do
-    OK.with do
-      %Nadia.Model.Update{
-        message: %Nadia.Model.Message{
-          text: text
-        }
-      } <- update
-    end
+    {:ok, update}
+      ~>> extractText
+      ~>> extractArgs
+      ~>> executeCommand(update)
+  end
 
-    case update do
-      %Nadia.Model.Update{
-        message: %Nadia.Model.Message{
-          text: text
-        }
-      } -> text
-      _ -> "default"
-    end
-      |> String.split(" ")
-      |> IO.inspect
+  defp executeCommand(args, update) do
+    IO.inspect args
+  end
+  
+  defp executeCommand(["help"], update)  do
+    send_message "halp"
+  end
+
+  defp extractArgs(text) do
+    [ _ | args] = String.split(text, " ")
+
+    {:ok, args}
   end
 
   defp extractText(update) do
@@ -30,8 +31,10 @@ defmodule Bot.Commands.Scheduler do
         message: %Nadia.Model.Message{
           text: text
         }
-      } -> text
-      _ -> "default"
+      } -> 
+        OK.success text
+      _ -> 
+        OK.failure :no_match  
     end
   end
 
